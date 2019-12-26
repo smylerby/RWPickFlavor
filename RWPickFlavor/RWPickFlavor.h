07e54cbf1bfd836bf0dc9e08a09b8bcc4cc7dfcf
Rustam Shorov <rustam.shorov@gismart.com>
2019-12-26 13:20:30 +0300

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Rustam Shorov on 12/26/19.
+//  Copyright © 2019 Rustam Shorov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
